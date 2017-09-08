a42fbf24e07bd26f18d4875e7576e62e779c0ab0
mtblue <mt.blue.lite@gmail.com>
2017-09-08 07:53:00 +0000

    mobile

--- wp-config.php
+++ wp-config.php
@@ -21,16 +21,16 @@ if (isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) && $_SERVER["HTTP_X_FORWARDED_PROT
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'c9');
+define('DB_NAME', 'heroku_1df4d302f2f4e4c');
 
 /** MySQL database username */
-define('DB_USER', substr(getenv('C9_USER'), 0, 16));
+define('DB_USER', 'b6a4cbbfca056a');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'd65a0062');
 
 /** MySQL hostname */
-define('DB_HOST', getenv('IP'));
+define('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');

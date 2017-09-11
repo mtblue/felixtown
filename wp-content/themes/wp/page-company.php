2132482ce912bfd30e7d35279d0147f691a346a0
mtblue <mt.blue.lite@gmail.com>
2017-09-11 06:54:38 +0000

    mobile

--- wp-content/themes/wp/page-company.php
+++ wp-content/themes/wp/page-company.php
@@ -3,8 +3,10 @@
 <div id="wrapper">
 	<div id="t02">
 		<div id="t02L">
-            <div class="ptitle">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/<?php echo $slug; ?>_title.gif" />
+            <div class="cm00">
+                <div class="ptitle">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/<?php echo $slug; ?>_title.gif" />
+                </div>
             </div>
             <div class="cm01">
                 <p>三幸住研の３つの理想概念をご紹介いたします。</p>
@@ -14,73 +16,105 @@
                 <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/company_img01.jpg" />
             </div>
             <div class="cm01">
-                <div class="ptitle2">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/outline_title.gif" />
+                <div class="cm00">
+                    <div class="ptitle2">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/outline_title.gif" />
+                	</div>
             	</div>
-            	<table id="outlineArea">
-                	<tr>
-                        	<th>会社名</th><td>株式会社 三幸住研</td>
-                        </tr>
+            	<div class="mobileHidden">
+                	<table id="outlineArea">
                     	<tr>
-                        	<th>所在地（本社）</th><td>〒560-0054　大阪府豊中市桜の町3丁目6-7</td>
-                        </tr>
-                    	<tr>
-                        	<th>TEL</th><td>06-4867-3000（代表）　06-6844-0105</td>
-                        </tr>
-                    	<tr>
-                        	<th>FAX</th><td>06-4867-3100（代表）　06-6844-1806</td>
-                        </tr>
-                        <tr>
-                        	<th>代表取締役</th><td>福原 昌樹</td>
-                        </tr>
-                    	<tr>
-                        	<th>会社創設</th><td>1971年7月19日</td>
-                        </tr>
-                    	<tr>
-                        	<th>資本金</th><td>1,000万円</td>
-                        </tr>
-                    	<tr>
-                        	<th>事業内容</th><td>一般建設業、建売業、不動産仲介業、賃貸業、不動産一般</td>
-                        </tr>
-                    	<tr>
-                        	<th>建設業者登録免許</th><td>大阪府知事許可(般-24)第97074</td>
-                        </tr>
-                    	<tr>
-                        	<th>宅地建物取引免許</th><td>大阪府知事免許(9)第25935</td>
-                        </tr>
-                    	<tr>
-                        	<th>その他登録</th><td>(財)住宅保証機構/登録番号21019061<br />(株)日本住宅保証検査機構(JIO/)登録番号A5000930</td>
-                        </tr>
-                    	<tr>
-                        	<th>所属団体</th><td>（社）大阪府宅地建物取引業協会 大阪支部<br />
+                            	<th>会社名</th><td>株式会社 三幸住研</td>
+                            </tr>
+                        	<tr>
+                            	<th>所在地（本社）</th><td>〒560-0054　大阪府豊中市桜の町3丁目6-7</td>
+                            </tr>
+                        	<tr>
+                            	<th>TEL</th><td>06-4867-3000（代表）　06-6844-0105</td>
+                            </tr>
+                        	<tr>
+                            	<th>FAX</th><td>06-4867-3100（代表）　06-6844-1806</td>
+                            </tr>
+                            <tr>
+                            	<th>代表取締役</th><td>福原 昌樹</td>
+                            </tr>
+                        	<tr>
+                            	<th>会社創設</th><td>1971年7月19日</td>
+                            </tr>
+                        	<tr>
+                            	<th>資本金</th><td>1,000万円</td>
+                            </tr>
+                        	<tr>
+                            	<th>事業内容</th><td>一般建設業、建売業、不動産仲介業、賃貸業、不動産一般</td>
+                            </tr>
+                        	<tr>
+                            	<th>建設業者登録免許</th><td>大阪府知事許可(般-24)第97074</td>
+                            </tr>
+                        	<tr>
+                            	<th>宅地建物取引免許</th><td>大阪府知事免許(9)第25935</td>
+                            </tr>
+                        	<tr>
+                            	<th>その他登録</th><td>(財)住宅保証機構/登録番号21019061<br />(株)日本住宅保証検査機構(JIO/)登録番号A5000930</td>
+                            </tr>
+                        	<tr>
+                            	<th>所属団体</th><td>（社）大阪府宅地建物取引業協会 大阪支部<br />
+                                北摂不動産事業協同組合<br />（公社）近畿地区不動産公正取引協議会<br />
+                                関西不動産情報センター<br />（一社）京阪神木造住宅協議会<br />ロイヤルハウス 北摂北店</td>
+                            </tr>
+                    </table>
+                </div>
+                <div class="pcHidden">
+                    <dl>
+                        <dt>会社名</dt><dd>株式会社 三幸住研</dd>
+                        <dt>所在地（本社）</dt><dd>〒560-0054　大阪府豊中市桜の町3丁目6-7</dd>
+                        <dt>TEL</dt><dd>06-4867-3000（代表）　06-6844-0105</dd>
+                        <dt>FAX</dt><dd>06-4867-3100（代表）　06-6844-1806</dd>
+                        <dt>代表取締役</dt><dd>福原 昌樹</dd>
+                        <dt>会社創設</dt><dd>1971年7月19日</dd>
+                        <dt>資本金</dt><dd>1,000万円</dd>
+                        <dt>事業内容</dt><dd>一般建設業、建売業、不動産仲介業、賃貸業、不動産一般</dd>
+                        <dt>建設業者登録免許</dt><dd>大阪府知事許可(般-24)第97074</dd>
+                        <dt>宅地建物取引免許</dt><dd>大阪府知事免許(9)第25935</dd>
+                        <dt>その他登録</dt><dd>(財)住宅保証機構/登録番号21019061<br />(株)日本住宅保証検査機構(JIO/)登録番号A5000930</dd>
+                        <dt>所属団体</dt>
+                        <dd>（社）大阪府宅地建物取引業協会 大阪支部<br />
                             北摂不動産事業協同組合<br />（公社）近畿地区不動産公正取引協議会<br />
-                            関西不動産情報センター<br />（一社）京阪神木造住宅協議会<br />ロイヤルハウス 北摂北店</td>
-                        </tr>
-                </table>
+                            関西不動産情報センター<br />（一社）京阪神木造住宅協議会<br />ロイヤルハウス 北摂北店
+                        </dd>
+                    </dl>
+                </div>
             </div>
             <div class="cm01">
-                <div class="ptitle2">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/map_title.gif" />
+                <div class="cm00">
+                    <div class="ptitle2">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/map_title.gif" />
+                    </div>
                 </div>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3276.155592274358!2d135.46136391523493!3d34.80202718040852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000fa8e7b96bb1f%3A0xd55625b827e5222e!2z44CSNTYwLTAwNTQg5aSn6Ziq5bqc6LGK5Lit5biC5qGc44Gu55S677yT5LiB55uu77yW4oiS77yX!5e0!3m2!1sja!2sjp!4v1500133663653" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-                <dl>
-                    <dt>
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/map_car.gif" />
-                    </dt>
-                    <dd><p>高速道路からお越しの方：名神自動車道吹田出口東に１５分</p></dd>
-                    <dd><p>一般道からのお越しの方：中央環状線桜の町交差点を箕面方面に曲がり、４００m程直進した右手</p></dd>
-                </dl>
-                <dl>
-                    <dt>
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/map_train.gif" />
-                    </dt>
-                    <dd><p>阪急電車からおお越しの方：阪急豊中駅から北緑丘団地方面行きバスで柴原町駅下車徒歩３分</p></dd>
-                    <dd><p>モノレールでお越しの方：柴原駅を東へ徒歩７分</p></dd>
-                </dl>
+                <div class="cm04">
+                    <dl>
+                        <dt>
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/map_car.gif" />
+                        </dt>
+                        <dd><p>高速道路からお越しの方：名神自動車道吹田出口東に１５分</p></dd>
+                        <dd><p>一般道からのお越しの方：中央環状線桜の町交差点を箕面方面に曲がり、４００m程直進した右手</p></dd>
+                    </dl>
+                </div>
+                <div class="cm04">
+                    <dl>
+                        <dt>
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/map_train.gif" />
+                        </dt>
+                        <dd><p>阪急電車からおお越しの方：阪急豊中駅から北緑丘団地方面行きバスで柴原町駅下車徒歩３分</p></dd>
+                        <dd><p>モノレールでお越しの方：柴原駅を東へ徒歩７分</p></dd>
+                    </dl>
+                </div>
             </div>
             <div class="cm01">
-                <div class="ptitle2">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/president_title.gif" />
+                <div class="cm00">
+                    <div class="ptitle2">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/president_title.gif" />
+                    </div>
                 </div>
                 <div class="cm03">
                     <div class="cm03l">
@@ -106,8 +140,10 @@
                 </div>
             </div>
             <div class="cm01">
-                <div class="ptitle2">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/staff_title.gif" />
+                <div class="cm00">
+                    <div class="ptitle2">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/staff_title.gif" />
+                    </div>
                 </div>
                 <ul>
                     <li>
@@ -129,8 +165,10 @@
                     </li>
                 </ul>
             </div>
-            <div class="cm02">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/contact_btn.jpg" />
+            <div class="mobileHidden">
+                <div class="cm02">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/contact_btn.jpg" />
+                </div>
             </div>
         </div>
     </div>
--- wp-content/themes/wp/page-trackrecord.php
+++ wp-content/themes/wp/page-trackrecord.php
@@ -1,55 +1,90 @@
 <?php get_header(); ?>
 <?php $slug = get_post($wp_query->post->ID)->post_name; ?>
 <div id="wrapper">
+    <div id="tw00">
 	<div id="t02">
 		<div id="t02L">
+		    <div class="pcHidden">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/sp/<?php echo $slug; ?>/<?php echo $slug; ?>_img01_sp.jpg" />
+            </div>
             <div class="ptitle">
                 <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/<?php echo $slug; ?>_title.gif" />
             </div>
             <div class="ts03">
-                <p>三幸住研の経験と実績をご紹介。</p>
+                <p class="mobileHidden">三幸住研の経験と実績をご紹介。</p>
             </div>
             <div class="tr01">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/trackrecord_img.jpg" />
-                <table>
-                    <tr>
-                        <th>豊中</th>
-                        <td>
-                            ■稲津　■永楽荘　■上野東　■上野西　■岡上の町　■小曽根　■春日町　■熊野町　■桜の町　■柴原町
-                            ■千里園　■刀根山　■服部西町　■本町　■緑ヶ丘　■宮山町　■向丘　■夕日丘　　　etc...	
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>池田／箕面／能勢</th>
-                        <td>
+                <img class="mobileHidden" src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $slug; ?>/trackrecord_img.jpg" />
+                <div class="mobileHidden">
+                    <table>
+                        <tr>
+                            <th>豊中</th>
+                            <td>
+                                ■稲津　■永楽荘　■上野東　■上野西　■岡上の町　■小曽根　■春日町　■熊野町　■桜の町　■柴原町
+                                ■千里園　■刀根山　■服部西町　■本町　■緑ヶ丘　■宮山町　■向丘　■夕日丘　　　etc...	
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>池田／箕面／能勢</th>
+                            <td>
+                            ■旭丘　■石橋　■石船　■瀬川　■畑　■畑野　■伏尾台　■平和台　■宿野　　　etc...
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>宝塚／川西／伊丹</th>
+                            <td>
+                            ■鋳物師　■鼓が滝　■清荒神　■宝塚南口　■多田院　■長寿ヶ丘　■つつじが丘　■錦松台　
+                            ■荻原台　■瑞穂　■水明台　■山本東　■矢問　　　etc...
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>西宮／神戸</th>
+                            <td>
+                            ■赤坂　■有野中町　■北六甲台　■神戸市北区上津台　■金仙寺　■さくら台　■塩瀬　■茶園町
+                            ■中央区坂口通　■中央区山本通　■藤原台　■平成台　■山口町　　　etc...
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>その他</th>
+                            <td>
+                            ■猪名川町旭ヶ丘　■亀岡市畑野町　■三田市あかしあ台　■三田市三田町　■三田市屋敷町
+                            ■枚方山之手町　■福島区海老江　■八尾市　　etc...
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="pcHidden">
+                    <dl>
+                        <dt>豊中</dt>
+                        <dd>
+                        ■稲津　■永楽荘　■上野東　■上野西　■岡上の町　■小曽根　■春日町　■熊野町　■桜の町　■柴原町
+                        ■千里園　■刀根山　■服部西町　■本町　■緑ヶ丘　■宮山町　■向丘　■夕日丘　　　etc...	
+                        </dd>
+                        <dt>池田／箕面／能勢</dt>
+                        <dd>
                         ■旭丘　■石橋　■石船　■瀬川　■畑　■畑野　■伏尾台　■平和台　■宿野　　　etc...
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>宝塚／川西／伊丹</th>
-                        <td>
+                        </dd>
+                        <dt>宝塚／川西／伊丹</dt>
+                        <dd>
                         ■鋳物師　■鼓が滝　■清荒神　■宝塚南口　■多田院　■長寿ヶ丘　■つつじが丘　■錦松台　
                         ■荻原台　■瑞穂　■水明台　■山本東　■矢問　　　etc...
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>西宮／神戸</th>
-                        <td>
+                        </dd>
+                        <dt>西宮／神戸</dt>
+                        <dd>
                         ■赤坂　■有野中町　■北六甲台　■神戸市北区上津台　■金仙寺　■さくら台　■塩瀬　■茶園町
                         ■中央区坂口通　■中央区山本通　■藤原台　■平成台　■山口町　　　etc...
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>その他</th>
-                        <td>
+                        </dd>
+                        <dt>その他</dt>
+                        <dd>
                         ■猪名川町旭ヶ丘　■亀岡市畑野町　■三田市あかしあ台　■三田市三田町　■三田市屋敷町
                         ■枚方山之手町　■福島区海老江　■八尾市　　etc...
-                        </td>
-                    </tr>
-                </table>
+                        </dd>
+                    </dl>
+                </div>
                 <p>その他、1,000件以上の実績！</p>
             </div>
         </div>
+    </div>
     </div>
 </div>
 <?php get_footer(); ?>
